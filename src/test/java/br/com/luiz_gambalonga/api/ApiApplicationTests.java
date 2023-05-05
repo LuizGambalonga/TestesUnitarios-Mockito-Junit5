4f54c8223aa97a596f853c785e0f7d620b1bcc24
LuizGambalonga <gambalongaluizhenrique@gmail.com>
2023-05-05 20:56:37 -0300

    Inicialização do Projeto

---
+++ src/test/java/br/com/luiz_gambalonga/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luiz_gambalonga.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
