a0aada4e4e342471aea9df25bf67b238aa94e76e
LuizGambalonga <gambalongaluizhenrique@gmail.com>
2023-05-05 21:20:54 -0300

    Teste

--- src/main/java/br/com/luiz_gambalonga/api/ApiApplication.java
+++ src/main/java/br/com/luiz_gambalonga/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.luiz_gambalonga.api;
 
+import br.com.luiz_gambalonga.api.domain.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+		Usuario usuario1 = new Usuario(1L,"LUIZ GAMBALONGA", "gamblonga@gmail.com","123");
+
+		System.out.println(usuario1);
 	}
 
 }
