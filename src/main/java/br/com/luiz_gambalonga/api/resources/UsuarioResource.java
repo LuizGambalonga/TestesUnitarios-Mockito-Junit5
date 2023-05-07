399895f966649fb3ecf348b4717278e8cd570a8e
LuizGambalonga <gambalongaluizhenrique@gmail.com>
2023-05-07 07:57:46 -0300

    Alterando metodo de Resource para ler direto no service.

--- src/main/java/br/com/luiz_gambalonga/api/resources/UsuarioResource.java
+++ src/main/java/br/com/luiz_gambalonga/api/resources/UsuarioResource.java
@@ -1,6 +1,8 @@
 package br.com.luiz_gambalonga.api.resources;
 
 import br.com.luiz_gambalonga.api.domain.Usuario;
+import br.com.luiz_gambalonga.api.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value ="/usuario")
 public class UsuarioResource {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Usuario> findByIdUsuario(@PathVariable Long id){
-        return ResponseEntity.ok().body(new Usuario(1L,"zezinhodafeira","zezote@gmail.com","123"));
+        return ResponseEntity.ok().body(usuarioService.findById(id));
     }
 }
