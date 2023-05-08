e734b76e8bd2541b3aca850dc3ad098e13f5198c
LuizGambalonga <gambalongaluizhenrique@gmail.com>
2023-05-08 15:03:50 -0300

    Teste para classe principal.

--- src/main/java/br/com/luiz_gambalonga/api/ApiApplication.java
+++ src/main/java/br/com/luiz_gambalonga/api/ApiApplication.java
@@ -1,6 +1,5 @@
 package br.com.luiz_gambalonga.api;
 
-import br.com.luiz_gambalonga.api.domain.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +8,6 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-
-		Usuario usuario1 = new Usuario(1L,"LUIZ GAMBALONGA", "gamblonga@gmail.com","123");
-
-		System.out.println(usuario1);
 	}
 
 }
